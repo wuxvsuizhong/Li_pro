2f4a4f52d9e79fda145f1b95d69185eba69ea32b
wuxvsuizhong <1558453997@qq.com>
2021-05-18 01:08:44 +0800

    20210518

--- py_demo/web/01-static_web_server.py
+++ py_demo/web/01-static_web_server.py
@@ -1,3 +1,5 @@
+#!coding=utf-8
+
 from socket import *
 from multiprocessing import Process
 import sys 
@@ -32,7 +34,7 @@ def main():
 	s = socket(AF_INET,SOCK_STREAM)
 	#s.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
 	try:
-		s.bind(("",8080))
+		s.bind(("",8000))
 	except OSError as err:
 		print('please wait %s'%str(err))
 		sys.exit()
@@ -48,41 +50,43 @@ def main():
 
 
 def doserver(client_sock,client_info):
-	#接受数据
-	recv_data = client_sock.recv(2048)
-	if len(recv_data) == 0:
-		client_sock.close()
-		return
-	recv_data = recv_data.decode('utf-8')
-	print(">>%s"%str(recv_data))
-	#解析HTTP头
-	data_list = recv_data.splitlines()
+    #接受数据
+    recv_data = client_sock.recv(2048)
+    if len(recv_data) == 0:
+        client_sock.close()
+        return
+    recv_data = recv_data.decode('utf-8')
+    print(">>%s"%str(recv_data))
+    #解析HTTP头
+    data_list = recv_data.splitlines()
 
-	#提取请请求路径
-	request_start_line = data_list[0]
-	file_name = re.match(r"\w+ +(/[^ ]*) ",request_start_line).group(1)
-	print('request file mame is %s'%str(file_name))
+    #提取请请求路径
+    request_start_line = data_list[0]
+    file_name = re.match(r"\w+ +(/[^ ]*) ",request_start_line).group(1)
+    print('request file mame is %s'%str(file_name))
 
-	if "/" == file_name:
-		res_request_data = res_data.encode('utf-8')#返回默认响应
-	elif fila_name.endwith(".py")#客户端请求执行脚本文件
-		#执行脚本
-		pass
-	else:
-		#返回请求响应数据_静态文件
-		try:
-			with open(HTTP_ROOT_DIR+file_name,'r') as read_data:
-				request_data = read_data.read()+'\r\n'
-		except IOError as err:
-			print('file error '+str(err))
-			res_request_data = error_res.encode('utf-8')
-		else:
-			#client_sock.send(normal_res)
-			res_request_data = normal_res.encode('utf-8')+request_data.encode('utf-8')
-		
-	ret = client_sock.send(res_request_data)	
-	print('ret = %d'%ret)		
-	client_sock.close()	
+    if "/" == file_name:
+        res_request_data = res_data.encode('utf-8')#返回默认响应
+    elif file_name.endswith(".py"):
+        #客户端请求执行脚本文件
+        #执行脚本
+        pass
+    else:
+        #返回请求响应数据_静态文件
+        try:
+            print("{}{}".format(HTTP_ROOT_DIR,file_name))
+            with open(HTTP_ROOT_DIR+file_name,'r') as read_data:
+                    request_data = read_data.read()+'\r\n'
+        except IOError as err:
+            print('file error '+str(err))
+            res_request_data = error_res.encode('utf-8')
+        else:
+            #client_sock.send(normal_res)
+            res_request_data = normal_res.encode('utf-8')+request_data.encode('utf-8')
+	    
+    ret = client_sock.send(res_request_data)	
+    print('ret = %d'%ret)		
+    client_sock.close()	
 
 
 if __name__ == '__main__':
